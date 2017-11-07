8aa83ae513fca0af5b108a3745a0590991318372
liuyu <15652294272@163.com>
2017-11-07 09:57:57 +0800

    ..

--- testDjano/settings.py
+++ testDjano/settings.py
@@ -37,15 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'testDjano',
     'django_crontab',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
-}
 
 CRONJOBS = [
     # 每隔十分钟运行一次
